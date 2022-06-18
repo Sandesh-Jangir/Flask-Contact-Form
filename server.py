de55008dc137795305ab11431bbe3aef5ec89efb
Sandesh Jangir <sandeshatgithub@gmail.com>
2022-06-18 18:10:07 +0530

    Compleated Version 1.0

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("Home.html")
+    return render_template("Home.html", options=['Query','Feedback','Complaint','Other'])
+@app.route("/result", methods=['GET', 'POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    comment_type = request.form['comment-type']
+    comment = request.form['comment']
+    return render_template("Result.html", data = [name,email,comment_type,comment])
 
 
 if __name__ == "__main__":
